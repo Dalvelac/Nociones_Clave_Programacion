3d4469125aefa6bd1771bfcd0c60bc278d840e1e
Daniel Alves Lacambra <dalvelac@myuax.com>
2024-10-09 15:31:57 +0000

    Actualizando organizacion de ejercicios

---
+++ Ejercicio1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hola Mundo!" << std::endl;
+
+    int a = 5;
+    int b = 15;
+
+    std::cout << "La suma de " << a << " + " << b << " es: " << a + b << std::endl;
+
+    int c = 30; 
+    std::cout << "El valor de c es: " << c << std::endl;
+    std::cout << "El valor de la suma a + b + c es: " << a + b + c << std::endl;
+
+    return 0; 
+}
